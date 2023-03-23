1b1e720f48d2467e23482db90ef1f62fd8b7e0d5
hrshankar2002 <hrshankar2002@gmail.com>
2023-03-23 19:21:04 +0530

    ✨ drawer complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,9 +34,84 @@ class _HomePageState extends State<HomePage> {
       ),
       body: _pages[_selectedIndex],
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.menu),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Column(
+                  children: [
+                    DrawerHeader(
+                        child: Image.asset(
+                      "lib/images/logo.png",
+                      color: Colors.white,
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Divider(
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 25.0),
+                      child: ListTile(
+                        leading: Icon(
+                          color: Colors.white,
+                          Icons.home,
+                        ),
+                        title: Text(
+                          'Home',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 25.0),
+                      child: ListTile(
+                        leading: Icon(
+                          color: Colors.white,
+                          Icons.info,
+                        ),
+                        title: Text(
+                          'About',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0, bottom: 25),
+              child: ListTile(
+                leading: Icon(
+                  color: Colors.white,
+                  Icons.logout,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
