95292ca6bde566cf20f849ee60184b87320781ad
hrshankar2002 <hrshankar2002@gmail.com>
2023-03-26 12:08:10 +0530

    ✨ shop page complete

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:movies_ui/components/shoe_tile.dart';
+import 'package:movies_ui/models/shoe.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -12,8 +14,78 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Shop Page'),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[100], borderRadius: BorderRadius.circular(8)),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 25),
+          child: Text('everyone flies... some fly longer than others'),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'See all',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  Shoe shoe = Shoe(
+                      name: 'Air Jordan',
+                      price: '240',
+                      imagePath: 'lib/images/img1.webp',
+                      description: 'Top Notch');
+                  return ShoeTile(
+                    shoe: shoe,
+                  );
+                })),
+        const Padding(
+          padding: EdgeInsets.only(top: 25, left: 25, right: 25),
+          child: Divider(
+            color: Colors.white,
+          ),
+        )
+      ],
     );
   }
 }
